d01041bf301f22317e0375c5cad12c7479fc3c1e
ayushdubey7799 <ayushdubey7799@gmail.com>
2023-05-21 14:55:36 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,13 @@ const App = () => {
     
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol>
+                {
+                  cityList
+                  .filter((city) => city.country === 'India')
+                  .map((city,index) => <li key={"location"+(index+1)}>{city.name}</li>)
+                }
+               </ol>
     </div>
   )
 }
